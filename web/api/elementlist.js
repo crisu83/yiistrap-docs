8f43fe6f8388487ede7559a1a611c2e6276da41d
Antonio Ramirez <amigo.cobos@gmail.com>
2013-07-17 10:10:20 +0200

    update api docs

--- web/api/elementlist.js
+++ web/api/elementlist.js
@@ -1,3 +1,3 @@
 
 var ApiGen = ApiGen || {};
-ApiGen.elements = [["c","TbActiveForm"],["c","TbAffix"],["c","TbAlert"],["c","TbApi"],["c","TbBreadcrumb"],["c","TbButtonColumn"],["c","TbCollapse"],["c","TbDataColumn"],["c","TbGridView"],["c","TbHeroUnit"],["c","TbHtml"],["c","TbListView"],["c","TbModal"],["c","TbNav"],["c","TbNavbar"],["c","TbPager"],["c","TbScrollspy"],["c","TbTabs"],["c","TbThumbnails"],["c","TbTypeAhead"],["c","TbWidget"]];
+ApiGen.elements = [["c","BootstrapCode"],["c","BootstrapGenerator"],["c","TbActiveForm"],["c","TbAffix"],["c","TbAlert"],["c","TbApi"],["c","TbBreadcrumb"],["c","TbButtonColumn"],["c","TbCollapse"],["c","TbDataColumn"],["c","TbDetailView"],["c","TbGridView"],["c","TbHeroUnit"],["c","TbHtml"],["c","TbListView"],["c","TbModal"],["c","TbNav"],["c","TbNavbar"],["c","TbPager"],["c","TbScrollspy"],["c","TbTabs"],["c","TbThumbnails"],["c","TbTypeAhead"],["c","TbWidget"]];
